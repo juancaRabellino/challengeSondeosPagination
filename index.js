457fa73afb0793c01ba037380a178121db6981ca
Juan Carlos Rabellino <juancarabellino@gmail.com>
2021-04-23 01:21:48 -0300

    Testing Final

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const paginate = function (items, page, itemsPerPage) {
-    /* YOUR CODE HERE */
-
+  
+  const itemsOnPage = []
+  let minPosition = page * itemsPerPage - itemsPerPage
+  let to = page * itemsPerPage > items.length ? items.length : page * itemsPerPage
+  for (let i = minPosition; i < to; i++) {
+    itemsOnPage.push(items[i])
+    console.log(items[i])
+  }
+  
+  return {
+    totalItems: items.length,
+    itemsOnPage,
+    page,
+    itemsPerPage,
+    from: minPosition + 1,
+    to,
+    prev: page > 1 ? true : false,
+    next: to < 16 ? true : false,
+  }
 }
 
 module.exports = paginate
